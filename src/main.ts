4ef830cf8acfb159bd2be4121ecb0970bfb202a3
Stas Mykhailenko <stas.m@2muchcoffee.com>
2019-05-13 18:38:05 +0300

    feat(app) create HttpExceptionsFilter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionsFilter } from './core/filters/http-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionsFilter());
+
   await app.listen(3000);
 }
 bootstrap();
