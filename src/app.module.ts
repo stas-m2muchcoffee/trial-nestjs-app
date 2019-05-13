6eece0a73436c7963baacf842fcb2f5e860cf838
Stas Mykhailenko <stas.m@2muchcoffee.com>
2019-05-13 13:43:27 +0300

    feat(app) import customers module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CustomersController } from './customers/customers.controller';
+
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/invoice-app-api')],
-  controllers: [AppController, CustomersController],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/invoice-app-api'),
+    CustomersModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
