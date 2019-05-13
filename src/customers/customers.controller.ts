b262cb0ec0c79fa59b2fd28057feb44a08b3b0e7
Stas Mykhailenko <stas.m@2muchcoffee.com>
2019-05-13 13:54:01 +0300

    feat(customers) create functionality to get all customers

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ICustomer } from './customer.interface';
+
+import { CustomersService } from './customers.service';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+
+  constructor(
+    private readonly customersService: CustomersService,
+  ) {
+  }
+
+  @Get()
+  getCustomers(): Promise<ICustomer[]> {
+    return this.customersService.getCustomersFromDb();
+  }
+}
--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Model } from 'mongoose';
+
+import { ICustomer } from './customer.interface';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+  @InjectModel('Customer')
+  private readonly customerModel: Model<ICustomer>;
+
+  getCustomersFromDb(): Promise<ICustomer[]> {
+    return this.customerModel.find({});
+  }
+}
