be6ea16104005e037c7b249afca71251d7a384ab
Stas Mykhailenko <stas.m@2muchcoffee.com>
2019-05-13 15:56:04 +0300

    feat(app) add useNewUrlParser option

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { CustomersModule } from './customers/customers.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/invoice-app-api'),
+    MongooseModule.forRoot('mongodb://localhost:27017/invoice-app-api', { useNewUrlParser: true }),
     CustomersModule,
   ],
   controllers: [AppController],
