e86d2167e653cfee4a3876e7503f70959a411dbc
Stas Mykhailenko <stas.m@2muchcoffee.com>
2019-05-14 12:01:47 +0300

    feat(customers:dto) update UpdateCustomerDto

--- src/customers/dto/update-customer.dto.ts
+++ src/customers/dto/update-customer.dto.ts
@@ -1,5 +1,15 @@
+import { IsOptional, IsPhoneNumber, IsString } from 'class-validator';
+
 export class UpdateCustomerDto {
+  @IsOptional()
+  @IsString()
   readonly name?: string;
+
+  @IsOptional()
+  @IsString()
   readonly address?: string;
+
+  @IsOptional()
+  @IsPhoneNumber('UA')
   readonly phone?: string;
 }
